6d6d399300fd4ff6d0a815c5c12ef41e6d5a8ec4
Moaz El Gandy <zezogameing1234@gmail.com>
2025-12-12 23:10:17 +0200

    chore: Delete file.

--- bot/src/middleware/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "telegraf";
-
-export const logger = async (ctx: Context, next: () => Promise<void>) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  const updateType = ctx.updateType;
-  console.log(`[${new Date().toISOString()}] Processing ${updateType} - ${ms}ms`);
-};
